7df77a5baafbf3305d98f5c5b157acc4fd9a18dd
s3152020 <155249961+s3152020@users.noreply.github.com>
2024-02-11 15:27:07 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,8 +1,62 @@
-$(document).ready(function(){
-    $("#alertButton").click(function(){
-      alert("Button was clicked");
-    });
+
+/*----------------------------------------------<Start> jQuery Call to a Function---------------------------------------*/
+function buttonTwoAlert() {
+  alert("This button does not do anything yet.");
+}
+/*----------------------------------------------<End> jQuery Call to a Function---------------------------------------*/
+
+
+/*----------------------------------------------<Start> Get a Random Emoji---------------------------------------*/
+function randomEmoji() {
+  for (i=0; i<3; i++) { 
+    randomRoll = Math.floor(Math.random()*3);
+
+    switch (randomRoll) {
+      case 0: getSymbol = "🍒"; break;
+      case 1: getSymbol = "🍋"; break;
+      case 2: getSymbol = "💲"; break;
+    }
+  }
+  return getSymbol;
+}
+
+/*----------------------------------------------<End> Get a Random Emoji---------------------------------------*/
+
+
+/*----------------------------------------------<Start> jQuery---------------------------------------*/
+
+$(document).ready(function() {
   
+  $("#betButtonOne").click(function(){ /*sets up three separate calls to get a random emoji then puts in the appropriate window*/
+    var box1 = randomEmoji();
+    var box2 = randomEmoji();
+    var box3 = randomEmoji();
+    $('#emojiBoxOne span').text(box1);
+    $('#emojiBoxTwo span').text(box2);
+    $('#emojiBoxThree span').text(box3);
+    alert("spinning...");
+    if (box1 == "🍒" && box2 == "🍒" && box3 == "🍒") {
+      alert("Cherry win");
+    }
+    if (box1 == "🍋" && box2 == "🍋" && box3 == "🍋") {
+      alert("Lemon win");
+    }
+    if (box1 == "💲" && box2 == "💲" && box3 == "💲") {
+      alert("Jackpot win");
+    }  
+  });
+  
+  $("#betButtonTwo").click(buttonTwoAlert); /*basic function to return an alert that this button isn't ready yet*/
+
+});
+
+/*----------------------------------------------<End> jQuery---------------------------------------*/
+
+
+
+
+/*
+$(document).ready(function(){
     $("#hideButton").click(function(){
       $("p").hide();
     });
@@ -28,3 +82,4 @@ $(document).ready(function(){
     });
    
 });
+*/
